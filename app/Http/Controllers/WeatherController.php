f69f40107a183deb976fc90739ccac6f40857461
Maciej Wiśniewski <m.wisniewski@elpromaelectronics.com>
2023-01-12 14:04:42 +0100

    Created a backend logic for application based on openweathermap API

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class WeatherController extends Controller
 {
-    //
+    public function index()
+    {
+        $client = new Client();
+        $apiKey = '4346aa1afacacb5c623f2a583f30e617';
+        $city = 'San Francisco';
+        $response = $client->get("http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=imperial");
+        $data = json_decode($response->getBody(), true);
+        
+        return view('weather', ['data' => $data]);
+    }
 }
